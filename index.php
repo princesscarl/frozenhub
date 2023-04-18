2589696f8d243c10f4607ce9a92bfc84b093538a
princesscarl <pcbaustria@iskolarngbayan.pup.edu.ph>
2023-04-18 16:07:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -89,7 +89,7 @@
           <div class="card-body">
             <h5 class="card-title">Card title</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Add to cart</a>
+            <a href="#" class="btn" style="background-color: #439D9E; color:white">Add to cart</a>
           </div>
         </div>
         <div class="row mt-5">
@@ -99,7 +99,7 @@
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Add to cart</a>
+                <a href="#" class="btn" style="background-color: #439D9E; color:white">Add to cart</a>
               </div>
             </div>
           </div>
@@ -111,7 +111,7 @@
           <div class="card-body">
             <h5 class="card-title">Card title</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Add to cart</a>
+            <a href="#" class="btn" style="background-color: #439D9E; color:white">Add to cart</a>
           </div>
         </div>
         <div class="row mt-5">
@@ -121,7 +121,7 @@
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Add to cart</a>
+                <a href="#" class="btn" style="background-color: #439D9E; color:white">Add to cart</a>
               </div>
             </div>
           </div>
@@ -133,7 +133,7 @@
           <div class="card-body">
             <h5 class="card-title">Card title</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Add to cart</a>
+            <a href="#" class="btn" style="background-color: #439D9E; color:white">Add to cart</a>
           </div>
         </div>
         <div class="row mt-5">
@@ -143,7 +143,7 @@
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Add to cart</a>
+                <a href="#" class="btn" style="background-color: #439D9E; color:white">Add to cart</a>
               </div>
             </div>
           </div>
@@ -155,7 +155,7 @@
           <div class="card-body">
             <h5 class="card-title">Card title</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Add to cart</a>
+            <a href="#" class="btn" style="background-color: #439D9E; color:white">Add to cart</a>
           </div>
         </div>
         <div class="row mt-5">
@@ -165,7 +165,7 @@
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Add to cart</a>
+                <a href="#" class="btn" style="background-color: #439D9E; color:white">Add to cart</a>
               </div>
             </div>
           </div>
@@ -173,10 +173,5 @@
       </div>
     </div>
   </div>
-  <footer style=" width: 90%; margin-top:10px; margin-bottom: 20px; margin-right: auto; margin-left: auto; background-color: #439D9E; border-radius: 5px;">
-    <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2); margin-top:20px; margin-bottom: 20px;  border-radius: 5px;">
-      © 2023 Copyright: <a class="text-white" href="https://Frozenhub.com/">Frozenhub.com</a>
-    </div>
-  </footer>
 </body>
 </html>

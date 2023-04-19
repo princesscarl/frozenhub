04222a79912850107653209a060f66bece40d649
DanCatapang <dbcatapang@iskolarngbayan.pup.edu.ph>
2023-04-19 15:23:03 +0800

    Update add_to_cart.php

--- add_to_cart.php
+++ add_to_cart.php
@@ -4,8 +4,16 @@ include '../connect.php';
    
         $user_id = $_SESSION['user_id'];
         $get_product_id = $_POST['product_id'];
+
+        $select_query = "SELECT * FROM cart_details WHERE  `user_id`= $user_id AND product_id=$get_product_id";
+        $count_query = mysqli_query($conn, $select_query);
+        $rows = mysqli_num_rows($count_query);
+
+        if ($rows == 0) {
+
         $insert_query = "INSERT INTO cart_details (product_id, quantity, `user_id`) VALUES ('$get_product_id','1', '$user_id')";
         $result_query = mysqli_query($conn, $insert_query);
+        }
 
         if($result_query){
             echo "<script> alert ('Item is added to cart.') </script>";

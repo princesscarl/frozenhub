8c1c4f281b6397ec76290cc5a87269c37081b0da
DanCatapang <dbcatapang@iskolarngbayan.pup.edu.ph>
2023-04-20 15:51:22 +0800

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -117,7 +117,7 @@ if (!$conn){
             echo '
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
-              <i class="fa fa-user"></i>
+              <i class="fa fa-user"></i></a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item" href="./user_area/login.php">Login</a>
                 <a class="dropdown-item" href="./user_area/register.php">Register</a>

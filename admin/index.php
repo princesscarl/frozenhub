eb2a78e5bcc0f2e8f784a01ad7c87f71b7ed5ece
DanCatapang <dbcatapang@iskolarngbayan.pup.edu.ph>
2023-04-25 11:57:45 +0800

    done

--- admin/index.php
+++ admin/index.php
@@ -43,39 +43,24 @@ if(isset($_POST["delete"]))
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   <script src="https://kit.fontawesome.com/faf8bee4ee.js" crossorigin="anonymous"></script>
   <script src="https://scripts.sirv.com/sirvjs/v3/sirv.js"></script>
+
+
   <script>
-    $('textarea').keyup(function() {
-    var characterCount = $(this).val().length,
-        current = $('#currentCount'),
-        maximum = $('#maxCount'),
-        theCount = $('#charCount');
-    current.text(characterCount);
-    
-    if (characterCount < 100) {
-        current.css('color', '#666');
-    }
-    if (characterCount > 100 && characterCount < 150) {
-        current.css('color', '#6d5555');
-    }
-    if (characterCount > 150 && characterCount < 180) {
-        current.css('color', '#793535');
-    }
-    if (characterCount > 180 && characterCount < 200) {
-        current.css('color', '#841c1c');
-    }
-    if (characterCount > 200 && characterCount < 250) {
-        current.css('color', '#8f0001');
-    }
-    if (characterCount >= 250) {
-        maximum.css('color', '#8f0001');
-        current.css('color', '#8f0001');
-        theCount.css('font-weight','bold');
-    } else {
-        maximum.css('color','#666');
-        theCount.css('font-weight','normal');
-    }
-});
-  </script>
+  $(document).ready(function(){
+function toggleDescription() {
+  var fullDescription = document.getElementById("full_description");
+  var viewMoreButton = document.getElementById("view_more_button");
+  
+  if (fullDescription.style.display === "none") {
+    fullDescription.style.display = "block";
+    viewMoreButton.innerHTML = "View Less";
+  } else {
+    fullDescription.style.display = "none";
+    viewMoreButton.innerHTML = "View More";
+  }
+}}
+  )
+</script>
 
 
   <style>
@@ -258,5 +243,7 @@ if(isset($_POST["delete"]))
     }
 });
   </script>
+
+
    </body>
 </html>
--- shop/common_functions.php
+++ shop/common_functions.php
@@ -21,16 +21,26 @@ function getproducts()
             $product_id = $row['product_id'];
             $product_title = $row['product_title'];
             $product_description = $row['product_description'];
+
+            $short_description = substr($product_description, 0, 25);
+
             $product_price = $row['product_price'];
             $product_image = $row['product_image'];
             echo "
 
   <div class='col-lg-3 col-md-3 col-sm-6 col-xs-1 d-flex justify-content-center mb-4'>
     <div class='card' style='width: 18rem;'>
-    <img src='./admin/products_images/$product_image' class='card-img-top'>
+    <img src='./admin/products_images/$product_image' class='card-img-top'  width='auto;' height='250px;'>
     <div class='card-body'>
     <h5 class='card-title'>$product_title</h5>
-    <p class='card-text'>$product_description</p>
+";?>
+ <div class="description">
+  <p><?php echo $short_description; ?></p>
+  <p class="full_description" style="display: none;"><?php echo $product_description; ?></p>
+  <button class="view_more_button" onclick="toggleDescription(this)">View More</button>
+</div>
+
+  <?php echo "
     <p class='card-text'><strong>Price:&nbsp$product_price</strong></p>"; ?>
 <?php
 
@@ -92,3 +102,4 @@ function getproducts()
 
 
 ?>
+

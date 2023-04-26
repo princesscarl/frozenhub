6526b065f1fb98c8eb0d501539a929f21eee097a
smbaylon <112595049+smbaylon@users.noreply.github.com>
2023-04-26 18:32:35 +0800

    Update all_products.php

--- shop/all_products.php
+++ shop/all_products.php
@@ -1,14 +1,14 @@
 <div class="container-fluid" style="width: 90%;">
     <h2 style="text-align: center; font-weight: bold; padding:10px;">All Products</h2>
 
-
-    <form class="justify-content-center my-2 d-flex" style="width:50%; " method="POST">
-      <!-- <input name="search_data" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <input type="submit" value="Search" class="btn btn-outline-light" name="search_data_product"> -->
-    <input type="text" name="all_products">
-    <button type="submit" class="btn btn-secondary ml-2" name="search">Search</button>
-    <a href="index.php?all_products" class="btn btn-info mx-2">Reset</a>
-    </form>
+<form class="form-inline mr-auto mb-3 d-flex  justify-content-end" >
+  <input class="form-control mt-2 mr-sm-2"  name="all_products" type="text" placeholder="Search" aria-label="Search" >
+  <button class="btn btn-unique btn-rounded btn-sm my-0 mt-2 mr-2"name="search" type="submit" style="background-color: #439D9E;">Search</button>
+  <a href="index.php?all_products" class="bi bi-arrow-counterclockwise mt-2 pl-2 pr-2" style="background-color: #439D9E; padding:3px;  color: white; border-radius: 3px; "></a>
+</form>
+    
+   
+   
 
     <div class='row d-flex'>
   

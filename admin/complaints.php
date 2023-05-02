ade8a4ace144d7d5c2be6222a0122fa4cf8f0c53
princesscarl <pcbaustria@iskolarngbayan.pup.edu.ph>
2023-05-02 11:06:29 +0800

    Update complaints.php

--- admin/complaints.php
+++ admin/complaints.php
@@ -45,7 +45,7 @@ $result = $conn->query($sql);
     <td data-label="Proposed Action/s">' . $row['actions'] . '</td>';
 
       ?>
-        <td><a href="../pictures/<?php echo $picture ?>">view file</a></td>
+        <td data-label="Pictures"><a href="../pictures/<?php echo $picture ?>">view file</a></td>
 
       <?php
         if ($status == "Pending") {

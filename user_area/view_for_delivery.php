c8ba267b03746ca573c5f655058b9b51ea42557e
DanCatapang <dbcatapang@iskolarngbayan.pup.edu.ph>
2023-05-02 11:23:39 +0800

    done

--- user_area/view_for_delivery.php
+++ user_area/view_for_delivery.php
@@ -68,7 +68,7 @@ if(isset($_SESSION['email']))
     <div class="container-fluid">
       <div class="row d-flex justify-content-center">
         <div class="col-lg-9">
-          <img src="https://lh3.googleusercontent.com/drive-viewer/AAOQEORwsoHzWxoEoNlJ4n1bR4aji_r7jA7WPbQOMth5REabik_rDa7pptnu1lFtHraszS04eNS4JYmXW5SNTKBZsK4H7D2vRg=s1600?fbclid=IwAR3h2V1bSwN87w2jWtMFhMPDet3eL-U8KSNIyguxKKjN3oRmny296FI5G8s" width="100%" height="auto">
+        <a href="../index.php">  <img src="https://lh3.googleusercontent.com/drive-viewer/AAOQEORwsoHzWxoEoNlJ4n1bR4aji_r7jA7WPbQOMth5REabik_rDa7pptnu1lFtHraszS04eNS4JYmXW5SNTKBZsK4H7D2vRg=s1600?fbclid=IwAR3h2V1bSwN87w2jWtMFhMPDet3eL-U8KSNIyguxKKjN3oRmny296FI5G8s" width="100%" height="auto"></a>
         
         </div>
       </div>

1419ab5c378eacbbc370fc45f716f1cd2975f6d7
DanCatapang <dbcatapang@iskolarngbayan.pup.edu.ph>
2023-05-05 13:00:30 +0800

    Update product_description.php

---
+++ shop/product_description.php
@@ -0,0 +1,34 @@
+<?php 
+include './connect.php';
+
+if(isset($_GET['product_description'])){
+ $id = $_GET['product_description'];
+
+
+ $products_query = "SELECT * FROM products  WHERE product_id = '$id'";
+ $result = mysqli_query($conn,$products_query);
+ $row = mysqli_fetch_assoc($result);
+
+ $product_image = $row['product_image'];
+ $product_title = $row['product_title'];
+ $product_price = $row['product_price'];
+ $product_description= $row['product_description'];
+}
+?>
+
+<div style="width: 90%; margin-top:30px; margin-bottom: 30px; margin-right: auto; margin-left: auto; overflow: hidden; background-color:white;">
+  <div class="row">
+    <div class="col-md-6">
+      <div class="product-image-column" style="max-width: auto; float: right;">
+        <img src="./admin/products_images/<?php echo $product_image ?>" alt="Product Image" style="width: 700px; height: 600px;">
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="product-description-column">
+        <h2><?php echo $product_title ?></h2>
+        <p><?php echo $product_price ?></p>
+        <p><?php echo $product_description ?></p>
+      </div>
+    </div>
+  </div>
+</div>

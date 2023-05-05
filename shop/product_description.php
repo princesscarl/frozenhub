5fd08864cbbd7afc214505efd18b7c8f741149ec
DanCatapang <dbcatapang@iskolarngbayan.pup.edu.ph>
2023-05-05 13:13:45 +0800

    Update product_description.php

--- shop/product_description.php
+++ shop/product_description.php
@@ -15,9 +15,15 @@ if(isset($_GET['product_description'])){
  $product_description= $row['product_description'];
 }
 ?>
+<div>
+<a href="index.php" style="margin-left:80px; color:black;">Home</a>   &nbsp/
+<a href="index.php?all_products" style="margin-left:10px; color:black;">All Products</a>  &nbsp/
+<a disabled href="#" style="margin-left:10px;color:black; text-decoratione-none;pointer-events: none;">Product Description</a>  
+</div>
+<div style="width: 90%; margin-top:30px; margin-bottom: 30px; margin-right: auto; margin-left: auto; overflow: hidden; background-color:teal;">
 
-<div style="width: 90%; margin-top:30px; margin-bottom: 30px; margin-right: auto; margin-left: auto; overflow: hidden; background-color:white;">
-  <div class="row">
+
+<div class="row">
     <div class="col-md-6">
       <div class="product-image-column" style="max-width: auto; float: right;">
         <img src="./admin/products_images/<?php echo $product_image ?>" alt="Product Image" style="width: 700px; height: 600px;">

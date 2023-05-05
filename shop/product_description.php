88c3981fa9de8a21065f354ff106b79147f11d9c
princesscarl <pcbaustria@iskolarngbayan.pup.edu.ph>
2023-05-05 16:20:10 +0800

    Update product_description.php

--- shop/product_description.php
+++ shop/product_description.php
@@ -42,7 +42,7 @@ if(isset($_GET['product_description'])){
         </div>  
 
 
-        <p class="price" style="margin-top:120px;">₱<?php echo $product_price ?></p>
+        <p class="price">₱<?php echo $product_price ?></p>
    
 
         

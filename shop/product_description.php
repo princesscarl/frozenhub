c5019acc8a5bf462e13a786ec4dc6c82f7a3d880
DanCatapang <dbcatapang@iskolarngbayan.pup.edu.ph>
2023-05-05 14:55:54 +0800

    ok

--- shop/product_description.php
+++ shop/product_description.php
@@ -15,24 +15,27 @@ if(isset($_GET['product_description'])){
  $product_description= $row['product_description'];
 }
 ?>
-<div>
+
 <a href="index.php" style="margin-left:80px; color:black;">Home</a>   &nbsp/
-<a href="index.php?all_products" style="margin-left:10px; color:black;">All Products</a>  &nbsp/
-<a disabled href="#" style="margin-left:10px;color:black; text-decoratione-none;pointer-events: none;">Product Description</a>  
-</div>
+<a href="#" onclick="history.back()" style="margin-left:10px; color:black;">Back to Products</a>  &nbsp/
+<a disabled href="#" style="margin-left:10px;color:gray;  text-decoratione-none;pointer-events: none;"><?php echo $product_title ?></a>  
+
+
+
+
 <div style="width: 90%; margin-top:30px; margin-bottom: 30px; margin-right: auto; margin-left: auto; overflow: hidden; background-color:teal;">
 
 
 <div class="row">
     <div class="col-md-6">
-      <div class="product-image-column" style="max-width: auto; float: right;">
-        <img src="./admin/products_images/<?php echo $product_image ?>" alt="Product Image" style="width: 700px; height: 600px;">
+      <div class="product-image-column" style="max-width: auto; float: center;">
+        <img src="./admin/products_images/<?php echo $product_image ?>" alt="Product Image">
       </div>
     </div>
     <div class="col-md-6">
       <div class="product-description-column">
         <h2><?php echo $product_title ?></h2>
-        <p><?php echo $product_price ?></p>
+        <p>₱<?php echo $product_price ?></p>
         <p><?php echo $product_description ?></p>
       </div>
     </div>

9ffc783e558bd5a8c14a1312f8c89ba8259f6272
DanCatapang <dbcatapang@iskolarngbayan.pup.edu.ph>
2023-05-08 15:08:34 +0800

    Update delete_products.php

--- admin/shop/delete_products.php
+++ admin/shop/delete_products.php
@@ -10,12 +10,10 @@ if (!$conn){
 if (isset($_GET['id'])){
     $product = $_GET['id'];
 
-    $delete_query = "DELETE FROM products WHERE product_id=$product";
-    $cart_delete_query = "DELETE FROM cart_details WHERE product_id=$product";
+    $delete_query = "DELETE FROM products WHERE product_code=$product";
     $result_delete= mysqli_query ($conn, $delete_query);
-    $result_cart = mysqli_query ($conn, $cart_delete_query);
 
-    if($result_cart && $cart_delete_query){
+    if($result_delete){
         echo "<script> alert ('Product is deleted!') </script>";
         header("Location: ../index.php?view_products");
     }
